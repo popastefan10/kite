a337d94e411659fd5f902596db19e12bb95b207e
Stefan Popa <popashtefan10@gmail.com>
2022-05-13 12:04:03 +0300

    SearchBar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,29 @@
-import { faCoffee, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "./SearchBar.css";
 
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <input type={"text"} placeholder={"Search..."} name={"search"}></input>
-        <button type="submit">
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
+      <div className={"search-bar-container"}>
+        <div className={"search-bar-title"}>
+          <h2>Locations</h2>
+        </div>
+        <div className="search-bar">
+          <input
+            type={"text"}
+            placeholder={"Search..."}
+            name={"search"}
+          ></input>
+          <button type="submit">
+            <FontAwesomeIcon
+              icon={faSearch}
+              className={"search-bar-icon"}
+              size={"xs"}
+            />
+          </button>
+        </div>
       </div>
     );
   }
