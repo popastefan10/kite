9a226df7ffbef10bc636e52b14874bcfee78ddfa
Stefan Popa <popashtefan10@gmail.com>
2022-05-14 19:22:08 +0300

    Now userID is saved in state and localStorage when you login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import postLogin from "./services/loginRequests";
 
 class App extends React.Component {
   constructor(props) {
@@ -15,6 +16,8 @@ class App extends React.Component {
     };
 
     this.handleDisplayedPageChange = this.handleDisplayedPageChange.bind(this);
+    this.handleUserLogin = this.handleUserLogin.bind(this);
+    this.handleUserLogout = this.handleUserLogout.bind(this);
   }
 
   handleDisplayedPageChange(newDisplayedPage) {
@@ -22,11 +25,32 @@ class App extends React.Component {
     this.setState({ displayedPage: newDisplayedPage });
   }
 
+  // Adds user data to state and local storage
+  handleUserLogin(userData) {
+    localStorage.setItem("userData", JSON.stringify(userData));
+    this.setState({ userId: userData.userId });
+  }
+
+  // Removes user data from state and local storage
+  handleUserLogout() {
+    localStorage.setItem("userData", null);
+    this.setState({ userId: null });
+  }
+
   // Conditional rendering
   getDisplayedPage() {
     if (this.state.displayedPage == "login")
-      return <Login onLogin={() => this.handleDisplayedPageChange("dashboard")} />;
-    return <Dashboard onLogout={() => this.handleDisplayedPageChange("login")}/>;
+      return (
+        <Login
+          onLogin={() => {
+            postLogin((userData) => this.handleUserLogin(userData));
+            this.handleDisplayedPageChange("dashboard");
+          }}
+        />
+      );
+    return (
+      <Dashboard onLogout={() => this.handleDisplayedPageChange("login")} />
+    );
   }
 
   render() {
