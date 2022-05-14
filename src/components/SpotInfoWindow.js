7147ec5b2024081b8b9a5519fee7492c0190ad09
Stefan Popa <popashtefan10@gmail.com>
2022-05-14 10:53:22 +0300

    Custom info window body styling

--- src/components/SpotInfoWindow.js
+++ src/components/SpotInfoWindow.js
@@ -11,7 +11,7 @@ class SpotInfoWindow extends React.Component {
       <div className={"spot-info-window"} style={{ backgroundColor: "white" }}>
         <div className="siw-head">
           <div className={"siw-head-left"}>
-            <h4 className="">{spot.name}</h4>
+            <h3 className="">{spot.name}</h3>
             <FontAwesomeIcon
               icon={faStar}
               size={"xs"}
@@ -25,36 +25,38 @@ class SpotInfoWindow extends React.Component {
           </div>
         </div>
 
-        <div className={"siw-country-container"}>
-          <p>{spot.country}</p>
-        </div>
+        <div className={"siw-body"}>
+          <div className={"siw-country-container"}>
+            <p>{spot.country}</p>
+          </div>
 
-        <div className="siw-info-list">
-          <ul>
-            <li>
-              <h6>Wind probability</h6>
-              <p>{spot.probability}</p>
-            </li>
-            <li>
-              <h6>Latitude</h6>
-              <p>{spot.lat}</p>
-            </li>
-            <li>
-              <h6>Longitude</h6>
-              <p>{spot.long}</p>
-            </li>
-            <li>
-              <h6>When to go</h6>
-              <p>{spot.month}</p>
-            </li>
-          </ul>
-        </div>
+          <div className="siw-info-list">
+            <ul>
+              <li>
+                <h5>WIND PROBABILITY</h5>
+                <p>{spot.probability}</p>
+              </li>
+              <li>
+                <h5>LATITUDE</h5>
+                <p>{spot.lat}</p>
+              </li>
+              <li>
+                <h5>LONGITUDE</h5>
+                <p>{spot.long}</p>
+              </li>
+              <li>
+                <h5>WHEN TO GO</h5>
+                <p>{spot.month}</p>
+              </li>
+            </ul>
+          </div>
 
-        <div className={"siw-favorites-button"}>
-          <button type="button">
-            <FontAwesomeIcon icon={faPlus} size={"xs"} />
-            <span>Add to favorites</span>
-          </button>
+          <div className={"siw-favorites-button"}>
+            <button type="button">
+              <FontAwesomeIcon icon={faPlus} size={"xs"} />
+              <span>Add to favorites</span>
+            </button>
+          </div>
         </div>
       </div>
     );
