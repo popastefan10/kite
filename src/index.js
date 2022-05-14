ea6d1dde20bdd55c71bf09f67e115e1649d232a8
Stefan Popa <popashtefan10@gmail.com>
2022-05-14 18:30:21 +0300

    Link to answer for the multiple LoadScript problem

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const API_KEY = "AIzaSyCFhIVkwY_rRptuPIZy7wjC_ZGw6MCTLTo";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* Why LoadScript should be here: https://github.com/JustFly1984/react-google-maps-api/issues/70#:~:text=edited-,Well%2C%20I%20managed%20to%20reuse%20it%20by%20loading%20the%20Loadscript%20inside%20the%20index.js%20wrapping%20all%20components.,-%F0%9F%91%8D */}
     <LoadScript googleMapsApiKey={API_KEY}>
       <App />
     </LoadScript>
