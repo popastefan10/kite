c713a27482ee09fbc7c8080dc64e4e3c4eb3b286
Stefan Popa <popashtefan10@gmail.com>
2022-05-14 13:18:00 +0300

    Added state for the user inputs' values

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,17 +2,48 @@ import React from "react";
 import "./LoginForm.css";
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "", // TODO: replace with encrypted password
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  // https://reactjs.org/docs/forms.html#handling-multiple-inputs
+  handleInputChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(`Input ${name} changed its value to ${value}`);
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
       <div className={"login-form-container"}>
         <div className={"user-input-container"}>
           <div className={"user-input username-input"}>
             <p>Username</p>
-            <input type={"text"} />
+            <input
+              type={"text"}
+              name={"username"}
+              value={this.state.name}
+              onChange={this.handleInputChange}
+            />
           </div>
           <div className={"user-input password-input"}>
             <p>Password</p>
-            <input type={"password"} />
+            <input
+              type={"password"}
+              name={"password"}
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
           </div>
         </div>
 
